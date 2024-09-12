2f7b6bb979dd57c6ea63aa05f9cc7928abca9d61
nghaan1810 <ngochan.tv253@gmail.com>
2024-09-12 21:33:06 +0700

    add choose date in daily report

--- lib/features/daily_report/views/daily_report_screen.dart
+++ lib/features/daily_report/views/daily_report_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hrm_aqtech/features/daily_report/controllers/daily_report_controller.dart';
+import 'package:hrm_aqtech/features/daily_report/views/widgets/choose_day_widget.dart';
 import 'package:hrm_aqtech/utils/constants/colors.dart';
 import 'package:hrm_aqtech/utils/constants/enums.dart';
 import 'package:hrm_aqtech/utils/helpers/hepler_function.dart';
@@ -11,6 +12,7 @@ class DailyReportScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(DailyReportController());
+    final TextEditingController chooseDate = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
@@ -31,8 +33,27 @@ class DailyReportScreen extends StatelessWidget {
       body: Obx(() {
         return Column(
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ChooseDateWidget(
+                    controller: chooseDate,
+                    label: "Chọn ngày",
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: MyColors.secondaryTextColor, width: 0.5),
+                    borderRadius: BorderRadius.circular(12)),
+                    child: IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.search, color: MyColors.secondaryTextColor,)))
+              ],
+            ),
             Padding(
-              padding: const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.only(bottom: 12, left: 12, right: 12),
               child: Container(
                 padding: const EdgeInsets.only(left: 20, right: 20),
                 decoration: BoxDecoration(
@@ -42,10 +63,7 @@ class DailyReportScreen extends StatelessWidget {
                 child: DropdownButton<Chart>(
                   value: controller.selectedChart.value,
                   underline: const SizedBox(),
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16
-                  ),
+                  style: const TextStyle(color: Colors.black, fontSize: 16),
                   isExpanded: true,
                   items: Chart.values.map((Chart chart) {
                     return DropdownMenuItem<Chart>(
