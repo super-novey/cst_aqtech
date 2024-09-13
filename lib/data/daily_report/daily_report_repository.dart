c9d7ee30cfffddf771bdff34471bc387fbeef738
super-novey <nganhduy2101003@gmail.com>
2024-09-13 09:08:59 +0700

    remove []

--- lib/data/daily_report/daily_report_repository.dart
+++ lib/data/daily_report/daily_report_repository.dart
@@ -1,4 +1,3 @@
-
 import 'package:hrm_aqtech/features/daily_report/models/aq_case_report_model.dart';
 import 'package:hrm_aqtech/features/daily_report/models/case_waiting_time_model.dart';
 import 'package:hrm_aqtech/features/daily_report/models/coder_case_report_model.dart';
@@ -8,10 +7,11 @@ import 'package:hrm_aqtech/utils/http/http_client.dart';
 class DailyReportRepository {
   Future<List<CaseWaitingTime>> fetchCaseWaitingTimeData() async {
     try {
-      final response = await HttpHelper.post("ThongKe/w-PhanBoSoCaseTheoThoiGianChoCoder", {});
+      final response = await HttpHelper.post(
+          "ThongKe/w-PhanBoSoCaseTheoThoiGianChoCoder", {});
       List<dynamic> data = response['data'] as List<dynamic>;
       return data.map((item) => CaseWaitingTime.fromJson(item)).toList();
-    }  on Exception catch (_) {
+    } on Exception catch (_) {
       rethrow;
     }
   }
@@ -19,8 +19,8 @@ class DailyReportRepository {
   Future<List<AqCaseReport>> fetchAQCaseReport() async {
     try {
       final request = {
-      "data": "2024-09-22T00:00:00.0000000", 
-    };
+        "data": "2024-09-22T00:00:00.0000000",
+      };
       final response = await HttpHelper.post("ThongKe/w-AqCaseReport", request);
       List<dynamic> data = response['data'] as List<dynamic>;
       return data.map((item) => AqCaseReport.fromJson(item)).toList();
@@ -32,9 +32,10 @@ class DailyReportRepository {
   Future<List<SupCaseReport>> fetchSupCaseReport() async {
     try {
       final request = {
-      "data": "2024-09-22T00:00:00.0000000", 
-    };
-      final response = await HttpHelper.post("ThongKe/w-supCaseReport", request);
+        "data": "2024-09-22T00:00:00.0000000",
+      };
+      final response =
+          await HttpHelper.post("ThongKe/w-supCaseReport", request);
       List<dynamic> data = response['data'] as List<dynamic>;
       return data.map((item) => SupCaseReport.fromJson(item)).toList();
     } on Exception catch (_) {
@@ -45,9 +46,10 @@ class DailyReportRepository {
   Future<List<CoderCaseReport>> fetchCoderCaseReport() async {
     try {
       final request = {
-      "data": "2024-09-22T00:00:00.0000000", 
-    };
-      final response = await HttpHelper.post("ThongKe/w-coderCaseReport", [request]);
+        "data": "2024-09-22T00:00:00.0000000",
+      };
+      final response =
+          await HttpHelper.post("ThongKe/w-coderCaseReport", request);
       List<dynamic> data = response['data'] as List<dynamic>;
       return data.map((item) => CoderCaseReport.fromJson(item)).toList();
     } on Exception catch (_) {
